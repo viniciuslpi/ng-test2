4f65f49d5443adf92249f291de3c2698415ca286
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-03-18 14:25:23 -0300

    new tests

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,5 +1,5 @@
 import { PhotoFrameModule } from './photo-frame.module';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
 
 describe(PhotoFrameComponent.name, () => {
@@ -18,6 +18,49 @@ describe(PhotoFrameComponent.name, () => {
 
     it(`Should create component`, () => {
         expect(component).toBeTruthy();
+    });
+
+    it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) once when called multiple times within debounce time`, fakeAsync(() => {
+        fixture.detectChanges();
+        let times = 0;
+        component.liked.subscribe(() => times++);
+        component.like();
+        component.like();
+        tick(500);
+        expect(times).toBe(1);
+    }));
+
+    it(`#${PhotoFrameComponent.prototype.like.name} should trigger (@Output liked) two times when callend outside debounce time`, fakeAsync(() => {
+        fixture.detectChanges();
+        let times = 0;
+        component.liked.subscribe(() => times++);
+        component.like();
+        tick(500);
+        component.like();
+        tick(500);
+        expect(times).toBe(2);
+    }));
+
+    it(`Should display number of likes when (@Input likes) is incremented`, () => {
+        fixture.detectChanges();
+        component.likes++;
+        fixture.detectChanges();
+        const element: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+        expect(element.textContent.trim()).toBe('1');
+    });
+
+    it(`Should update aria-label when (@Input likes) is incremented`, ()=> {
+        fixture.detectChanges();
+        component.likes++;
+        fixture.detectChanges();
+        const element: HTMLElement = fixture.nativeElement.querySelector('span');
+        expect(element.getAttribute('aria-label')).toBe('1: people liked');
+    });
+
+    it(`Should have aria-label with 0 (@Input likes) value`, () => {
+        fixture.detectChanges();
+        const element: HTMLElement = fixture.nativeElement.querySelector('span');
+        expect(element.getAttribute('aria-label')).toBe('0: people liked');
     })
 
-})
+});
