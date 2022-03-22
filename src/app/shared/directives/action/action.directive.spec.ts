bb18f45ec3e2a801a6e78abdaacb97d438bfc3d7
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-03-22 17:01:56 -0300

    new directive tests

---
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -0,0 +1,45 @@
+import { ActionDirectiveModule } from './action.module';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ActionDirective } from './action.directive'
+import { Component } from '@angular/core';
+
+describe(ActionDirective.name, () => {
+
+  let fixture: ComponentFixture<ActionDirectiveTestComponent>;
+  let component: ActionDirectiveTestComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ActionDirectiveTestComponent],
+      imports: [ActionDirectiveModule]
+    }).compileComponents();
+  });
+
+  fixture = TestBed.createComponent(ActionDirectiveTestComponent);
+  component = fixture.componentInstance;
+
+  it(`(D) (@Output appAction) should emit event with payload when ENTER key is pressed`, () => {
+    const divEl: HTMLElement = fixture.nativeElement.querySelector('.dummy-component');
+    const event = new KeyboardEvent('keyup', {key: 'Enter'});
+    divEl.dispatchEvent(event);
+    expect(component.hasEvent()).toBe(true);
+  })
+
+});
+
+@Component({
+  template: `<div class="dummy-component" (appAction)="actionHandler($event)"></div>`
+})
+class ActionDirectiveTestComponent{
+
+  private event: Event = null;
+
+  actionHandler(event: Event): void{
+    this.event = event;
+  }
+
+  hasEvent(): boolean{
+    return !!this.event; // !! converte o tipo pra boolean
+  }
+
+}
