d8fcc1f03c2febc533ef3952b1758641df9636b6
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-04-01 14:00:35 -0300

    using mock provider for tests

--- src/app/components/photo-list/photo-list.component-mock-provider.spec.ts
+++ src/app/components/photo-list/photo-list.component-mock-provider.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Observable, of } from 'rxjs';
 import { Photo } from 'src/app/shared/components/photo-board/interfaces/photo';
+import { PhotoBoardMockService } from 'src/app/shared/components/photo-board/services/photo-board-mock.service';
 import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
 import { buildPhotoList } from 'src/app/shared/components/photo-board/test/build-photo-list';
 import { PhotoListComponent } from './photo-list.component';
@@ -21,11 +22,7 @@ describe(PhotoListComponent.name + ' Mock Provider', () => {
             providers: [
                 {
                     provide: PhotoBoardService,
-                    useValue: {
-                        getPhotos(): Observable<Photo[]> {
-                            return of(buildPhotoList())
-                        }
-                    }
+                    useClass: PhotoBoardMockService // função mockada dentro da class
                 }
             ]
         }).compileComponents();
